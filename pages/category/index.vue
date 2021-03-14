5b4dbcfdeb1eadf6d27704e3253b160f98353847
Hanna <hanna@MacBook-Air-Hanna.local>
2021-03-14 15:13:48 +0300

    init

---
+++ pages/category/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Card :resources="resultQuery" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categoryQuery: this.$route.query.category,
+      allResources: [
+        { title: "BMV", category: "car" },
+        { title: "Opel", category: "car" },
+        { title: "Toyota", category: "car" },
+        { title: "banana", category: "fruts" },
+        { title: "apple", category: "fruts" },
+        { title: "orange", category: "fruts" },
+        { title: "hazelnut", category: "nut" },
+        { title: "almond", category: "nut" },
+        { title: "seeds", category: "nut" },
+      ],
+    };
+  },
+  computed: {
+    resultQuery() {
+      if (this.categoryQuery) {
+        return this.allResources.filter((item) => {
+          return this.categoryQuery
+            .toLowerCase()
+            .split(" ")
+            .every(
+              (v) =>
+                item.category.toLowerCase().includes(v)
+            );
+        });
+      } else {
+        return this.allResources;
+      }
+    },
+  },
+};
+</script>
